289c05c56b864d052c28abed8d57a3cbb11ddd1f
carsonjohnson <carson74johnson@gmail.com>
2024-08-25 12:22:32 -0400

    fixed f-string

--- utils/job_input_view.py
+++ utils/job_input_view.py
@@ -45,6 +45,8 @@ class JobInputView(View):
         
         # Confirm and process
         await self.message.channel.send(f"Thank you! Here’s what you provided:\n\n**Job Title**: {job_title.content}\n**Company**: {company.content}\n**Minimum Qualifications**: {min_qual.content}\n**Preferred Qualifications**: {pref_qual.content}")
+        
+        return
 
     async def no_button_callback(self, interaction: discord.Interaction):
         interaction.data['custom_id'] == 'no'
